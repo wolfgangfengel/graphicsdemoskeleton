5c03b88bd2004ff8e2895bad86906eab16b2eab5
wolfgang.engel <wolfgang.engel@da5beada-464d-0410-a84d-2b73e14d68f6>
2013-11-07 01:57:32 +0000

    latest code now I can compile and store a compressed shader binary and open it later. 1395 bytes in size ... when the pre-compiled shader is used.

--- DirectCompute/Window.c
+++ DirectCompute/Window.c
@@ -288,8 +288,10 @@ __declspec( naked )  void __cdecl winmain()
 	//
 	ID3DBlob *pByteCodeBlob = NULL;
 	ID3DBlob *pErrorBlob = NULL;
-//	ID3DBlob *pCompressedByteCodeBlob = NULL;
+	ID3DBlob *pCompressedByteCodeBlob = NULL;
 	ID3D11ComputeShader *pCompiledComputeShader = NULL;
+	D3D_SHADER_DATA ShaderData;
+
 
 #ifdef COMPILENWRITEOUTSHADERS
  	// HRESULT hr = D3DX11CompileFromFile( "qjulia4D.hlsl", NULL, NULL, "CS_QJulia4D", "cs_5_0", 0, 0, NULL, &pByteCodeBlob, &pErrorBlob, NULL);
@@ -309,25 +311,23 @@ __declspec( naked )  void __cdecl winmain()
 	if(hr != S_OK)
 		MessageBoxA(NULL, (char *)pErrorBlob->lpVtbl->GetBufferPointer(pErrorBlob), "Error", MB_OK | MB_ICONERROR);
 
-/*	D3D_SHADER_DATA ShaderData;
-			ShaderData.pBytecode = pByteCodeBlob->lpVtbl->GetBufferPointer(pByteCodeBlob);
-			ShaderData.BytecodeLength = pByteCodeBlob->lpVtbl->GetBufferSize(pByteCodeBlob);
+	ShaderData.pBytecode = pByteCodeBlob->lpVtbl->GetBufferPointer(pByteCodeBlob);
+	ShaderData.BytecodeLength = pByteCodeBlob->lpVtbl->GetBufferSize(pByteCodeBlob);
 
 	hr = D3DCompressShaders(1, &ShaderData, D3D_COMPRESS_SHADER_KEEP_ALL_PARTS, &pCompressedByteCodeBlob);
 	if(hr != S_OK)
 		MessageBoxA(NULL, "D3DCompressShaders() failed", "Error", MB_OK | MB_ICONERROR);
-*/	
+	
 	// seems to require DirectX 11.1
-	//D3DWriteBlobToFile(&pByteCodeBlob, "ComputeShader.sh", TRUE);
+	D3DWriteBlobToFile(pCompressedByteCodeBlob, L"ComputeShader.sh", TRUE);
 #endif
 
-
 	// seems to require DirectX 11.1
-	//D3DReadFileToBlob("ComputeShader.sh", &pByteCodeBlob);
+	D3DReadFileToBlob(L"ComputeShader.sh", &pCompressedByteCodeBlob);
 
 	// compressed version seems to be 571 in size while the uncompressed version is 776
-//	unsigned int sizeOfCompressedBlob = pCompressedByteCodeBlob->lpVtbl->GetBufferSize(pCompressedByteCodeBlob);
-//	D3DDecompressShaders(pCompressedByteCodeBlob->lpVtbl->GetBufferPointer(pCompressedByteCodeBlob), sizeOfCompressedBlob, 1, 0, NULL, 0, &pByteCodeBlob, NULL);
+	unsigned int sizeOfCompressedBlob = pCompressedByteCodeBlob->lpVtbl->GetBufferSize(pCompressedByteCodeBlob);
+	D3DDecompressShaders(pCompressedByteCodeBlob->lpVtbl->GetBufferPointer(pCompressedByteCodeBlob), sizeOfCompressedBlob, 1, 0, NULL, 0, &pByteCodeBlob, NULL);
 
 #ifdef COMPILENWRITEOUTSHADERS
 	hr =
