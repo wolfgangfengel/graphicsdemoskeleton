562ec246941c0f46e5e125a3c2a6333cf0daeed9
wolfgang.engel <wolfgang.engel@da5beada-464d-0410-a84d-2b73e14d68f6>
2014-01-13 00:34:55 +0000

    another idea how to compile shaders ... it is too big for small size demos ..

--- UsefulCodeSnippets/CompilingComputeShaders.c
+++ UsefulCodeSnippets/CompilingComputeShaders.c
@@ -42,3 +42,58 @@
 
 	if(hr != S_OK)
 		MessageBoxA(NULL, "CreateComputerShader() failed", "Error", MB_OK | MB_ICONERROR);
+		
+		
+Other idea (too big):
+
+const char *ColorFilter =
+"StructuredBuffer<float4> C:register(t0);"
+"RWTexture2D<float4> c:register(u0);\n"
+"#define THREADX 16\n"
+"#define THREADY 16\ncbuffer cbCS:register(b0){uint c_height:packoffset(c0.x);uint c_width:packoffset(c0.y);float Saturation:packoffset(c7.y);float ColorCorrectRed:packoffset(c7.z);float ColorCorrectGreen:packoffset(c7.w);float ColorCorrectBlue:packoffset(c8.x);float ColorAddRed:packoffset(c8.y);float ColorAddGreen:packoffset(c8.z);float ColorAddBlue:packoffset(c8.w);float3 Contrast:packoffset(c9);};"
+"groupshared float3 f[THREADX*THREADY];"
+"[numthreads(THREADX,THREADY,1)]"
+"void t(uint3 t:SV_GroupID,uint3 n:SV_DispatchThreadID,uint3 e:SV_GroupThreadID,uint T:SV_GroupIndex)"
+"{"
+"uint u=n.x+n.y*c_width;"
+"f[T]=C[u].xyz;"
+"GroupMemoryBarrierWithGroupSync();"
+"float3 D;"
+"if(u<c_width*c_height-1)"
+"{"
+"float3 l=f[T],d=l-Contrast*(l-1.f)*l*(l-.5f);"
+"float S=dot(d,float3(.2126,.7152,.0722));"
+"float3 h=lerp(S.xxx,d,Saturation);"
+"D=h*float3(ColorCorrectRed,ColorCorrectGreen,ColorCorrectBlue)*float3(2.f,2.f,2.f)+float3(ColorAddRed,ColorAddGreen,ColorAddBlue);"
+"}"
+"c[n.xy]=float4(D,1.);"
+"}";
+
+
+	//
+	// compile the compute shaders
+	//
+	ID3D11ComputeShader *pCompiledComputeShader;
+	ID3D11ComputeShader *pCompiledPostFXComputeShader;
+
+#if defined(_DEBUG)
+	hr =
+#endif
+	pd3dDevice->lpVtbl->CreateComputeShader(pd3dDevice, g_CS_QJulia4D, sizeof(g_CS_QJulia4D), NULL, &pCompiledComputeShader);
+
+	ID3DBlob *pByteCodeBlob = NULL;
+	ID3DBlob *pErrorBlob = NULL;
+
+#if defined(_DEBUG)
+	hr =
+#endif
+	// seems to require DirectX 11.1  
+	//D3DCompileFromFile(ColorFilter, NULL, NULL, "t", "cs_5_0", D3DCOMPILE_OPTIMIZATION_LEVEL3, 0, &pByteCodeBlob, &pErrorBlob);
+	D3DCompile2(ColorFilter, sizeof(char) * 1044, NULL, NULL, NULL, "t", "cs_5_0", D3DCOMPILE_OPTIMIZATION_LEVEL3, 0, 0, NULL, NULL, &pByteCodeBlob, &pErrorBlob);
+
+
+#if defined(_DEBUG)
+	if (hr != S_OK)
+		MessageBoxA(NULL, (char *)pErrorBlob->lpVtbl->GetBufferPointer(pErrorBlob), "Error", MB_OK | MB_ICONERROR);
+#endif
+		
