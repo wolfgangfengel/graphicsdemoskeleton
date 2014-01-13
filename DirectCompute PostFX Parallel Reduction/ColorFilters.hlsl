0d5144ff64b29c805fc679bca4e5b8d0172c7fee
wolfgang.engel <wolfgang.engel@da5beada-464d-0410-a84d-2b73e14d68f6>
2014-01-13 00:31:52 +0000

    First Parallel Reduction example ...

---
+++ DirectCompute PostFX Parallel Reduction/ColorFilters.hlsl
@@ -0,0 +1,122 @@
+////////////////////////////////////////////////////////////////////////
+//
+// Instructional Post-Processing Color filters with DirectCompute
+//
+// by Wolfgang Engel 
+//
+// Last time modified: 12/31/2013
+//
+///////////////////////////////////////////////////////////////////////
+StructuredBuffer<float4> Input : register( t0 );
+RWTexture2D<float4> Result : register (u0);
+
+#define THREADX 16
+#define THREADY 16
+
+cbuffer cbCS : register(b0)
+{
+	int c_height : packoffset(c0.x);
+	int c_width : packoffset(c0.y);		// size view port
+/*	
+	This is in the constant buffer as well but not used in this shader, so I just keep it in here as a comment
+	
+	float c_epsilon : packoffset(c0.z);	// julia detail  	
+	int c_selfShadow : packoffset(c0.w);  // selfshadowing on or off  
+	float4 c_diffuse : packoffset(c1);	// diffuse shading color
+	float4 c_mu : packoffset(c2);		// julia quaternion parameter
+	float4x4 rotation : packoffset(c3);
+	float zoom : packoffset(c7.x);
+*/
+	float Saturation : packoffset(c7.y);
+	float ColorCorrectRed : packoffset(c7.z);
+	float ColorCorrectGreen : packoffset(c7.w);
+	float ColorCorrectBlue : packoffset(c8.x);
+	float ColorAddRed : packoffset(c8.y);
+	float ColorAddGreen : packoffset(c8.z);
+	float ColorAddBlue : packoffset(c8.w);
+	float3 Contrast : packoffset(c9);
+};
+
+//
+// the following shader stores 256 values in thread group shared memory by copying them from structured buffer
+// it applies then color filter operations and copies the data then into RWTexture2D
+//
+#define groupthreads THREADX * THREADY
+groupshared float4 sharedMem[groupthreads];
+
+// SV_DispatchThreadID - index of the thread within the entire dispatch in each dimension: x - 0..x - 1; y - 0..y - 1; z - 0..z - 1
+// SV_GroupID - index of a thread group in the dispatch — for example, calling Dispatch(2,1,1) results in possible values of 0,0,0 and 1,0,0, varying from 0 to (numthreadsX * numthreadsY * numThreadsZ) – 1
+// SV_GroupThreadID - 3D version of SV_GroupIndex - if you specified numthreads(3,2,1), possible values for the SV_GroupThreadID input value have the range of values (0–2,0–1,0)
+// SV_GroupIndex - index of a thread within a thread group
+[numthreads(THREADX, THREADY, 1)]
+void PostFX( uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex  )
+{
+	const float4 LumVector = float4(0.2125f, 0.7154f, 0.0721f, 0.0f);
+
+	// read from structured buffer
+	uint idx = DTid.x + DTid.y * c_width;
+
+	// store in shared memory    
+	sharedMem[GI] = dot(Input[idx], LumVector);
+
+	// Parallel reduction algorithm follows 
+	GroupMemoryBarrierWithGroupSync();
+
+	// Parallel reduction
+	[unroll(groupthreads)]
+
+	// 
+	for (uint s = groupthreads / 2; s > 0; s >>= 1)
+	{
+		if (GI < s)
+			sharedMem[GI] += sharedMem[GI + s];
+
+		GroupMemoryBarrierWithGroupSync();
+	}
+
+	/*
+	// Parallel reduction algorithm follows
+	// add up 64..127 to 0..63
+	if ( GI < 64 )
+	accum[GI] += accum[64+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 32 )
+	// add up 32..63 to 0..31
+	accum[GI] += accum[32+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 16 )
+	// add up 16..31 to 0..15
+	accum[GI] += accum[16+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 8 )
+	// add up 8..15 to 0..7
+	accum[GI] += accum[8+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 4 )
+	// add up 4..7 to 0..3
+	accum[GI] += accum[4+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 2 )
+	// add up 2..3 to 0..1
+	accum[GI] += accum[2+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	if ( GI < 1 )
+	// add up 1 to 0
+	accum[GI] += accum[1+GI];
+	GroupMemoryBarrierWithGroupSync();
+
+	*/
+
+	// Have the first thread write out to the output
+	if (GI == 0)
+	{
+		// write out the result for each thread group
+		Result[Gid.xy] = sharedMem[0] / (THREADX * THREADY);
+	}
+}
